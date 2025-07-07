71df40f241122b8ff90418272bc44a7ad4130436
Phước Tín <balun19772018@gmail.com>
2025-07-07 11:55:34 +0700

    update css-js-html

--- asset/js/details-protduct.js
+++ asset/js/details-protduct.js
@@ -1,12 +1,34 @@
-function changeQuantity(amount) {
+function khongTimThaySanPham() {
+  document.getElementById("productNotFound").style.display = "block";
+  document.getElementsByClassName("chitietSanpham")[0].style.display = "none";
+}
+
+function increaseQty() {
   const input = document.getElementById("quantity");
-  let currentValue = parseInt(input.value) || 1;
-  let newValue = currentValue + amount;
-
-  const min = parseInt(input.min);
-  const max = parseInt(input.max);
-
-  if (newValue >= min && newValue <= max) {
-    input.value = newValue;
+  let value = parseInt(input.value) || 0;
+  if (value < 999) {
+    input.value = value + 1;
   }
 }
+
+function decreaseQty() {
+  const input = document.getElementById("quantity");
+  let value = parseInt(input.value) || 0;
+  if (value > 1) {
+    input.value = value - 1;
+  }
+}
+
+// Cập nhật sao
+var rating = "";
+if (sanPhamHienTai.rateCount > 0) {
+  for (var i = 1; i <= 5; i++) {
+    if (i <= sanPhamHienTai.star) {
+      rating += `<i class="fa fa-star"></i>`;
+    } else {
+      rating += `<i class="fa fa-star-o"></i>`;
+    }
+  }
+  rating += `<span> ` + sanPhamHienTai.rateCount + ` đánh giá</span>`;
+}
+divChiTiet.getElementsByClassName("rating")[0].innerHTML += rating;
